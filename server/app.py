09b784f392b822d0a8168cd0c3c039497cd57d23
hphuc15 <hphuc15@outlook.com.vn>
2025-09-18 21:35:49 +0700

    chore(env): migrate project from Windows to WSL Ubuntu and configure MariaDB

--- server/app.py
+++ server/app.py
@@ -1,9 +1,23 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, g
 from datetime import datetime, timezone
+import sqlite3
 
 app = Flask(__name__)
 
-data = [] # Fake database
+DATABASE = r"D:\WorkSpace\Projects\LightSense_IoT\database\mydb.sqlite"
+# data = [] # Fake database
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close() 
 
 @app.route("/")
 def home():
