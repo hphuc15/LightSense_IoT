674d97490d3410bd2a662176f6187b58bd5bd9d5
hphuc15 <hphuc15@outlook.com.vn>
2025-09-21 17:25:21 +0700

    refactor(routes): update route receive data and display data

--- server/app.py
+++ server/app.py
@@ -1,44 +1,59 @@
-from flask import Flask, request, render_template, jsonify, g
+from flask import Flask, request, render_template, jsonify
 from datetime import datetime, timezone
-import sqlite3
+import mysql.connector
 
 app = Flask(__name__)
 
-DATABASE = r"/home/hphuc15/Workspace/Project/LightSense_IoT/database/mydb.sqlite"
-# data = [] # Fake database
-
-def get_db():
-    if "db" not in g:
-        g.db = sqlite3.connect(DATABASE)
-        g.db.row_factory = sqlite3.Row
-    return g.db
-
-@app.teardown_appcontext
-def close_db(exception):
-    db = g.pop("db", None)
-    if db is not None:
-        db.close() 
 
 @app.route("/")
 def home():
     return "<h1>Homepage</h1>"
 
+
 @app.route("/api/data", methods=["POST"])
 def receive_data():
     new_data = request.get_json()
-    if new_data:
-        record = {
-            "timestamp": datetime.now(timezone.utc).isoformat(),
-            "data": new_data
-        }
-        data.append(record)
-        return '{"status": "success"}'
-    return '{"status": "error"}', 400
+    if not new_data:
+        return '{"status": "record failed"}', 400
+    
+    record = {
+        "timestamp": datetime.now(timezone.utc),
+        "lux": new_data["lux"]
+    }
 
-@app.route("/history")
+    with mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "hongphucv9@",
+        database = "bh1750_db"
+    ) as conn:
+        with conn.cursor() as curs:
+            insert_query = "INSERT INTO data (timestamp, lux) VALUES (%s, %s)"
+            insert_query_argument = (record["timestamp"], record["lux"])
+            curs.execute(insert_query, insert_query_argument)
+            conn.commit()
+
+    return '{"status": "record succes"}'
+
+
+@app.route("/history", methods=["GET"])
 def get_data():
-    return render_template("data.html", data=data)
-# Test: $ curl -X POST http://192.168.1.13:5000/api/data -H "Content-Type: application/json" -d '{"temperature": 25, "humidity": 60}'
+    with mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "hongphucv9@",
+        database = "bh1750_db"
+    ) as conn:
+        with conn.cursor(dictionary = True) as curs:
+            select_query = "SELECT timestamp, lux FROM data ORDER BY timestamp DESC"
+            curs.execute(select_query)
+            records = curs.fetchall()
+
+    return render_template("data.html", records=records)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+
+# record: a single entry to INSERT into the database
+# records: a list of entries fetched from the database
